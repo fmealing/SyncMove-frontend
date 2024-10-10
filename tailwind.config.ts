f3a24ecdc462dbf9beacfc87a6cf4f4ac28a189f
Florian Mealing <florianmealing@gmail.com>
2024-10-10 20:34:23 +0100

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,38 @@
-import type { Config } from "tailwindcss";
+import { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#007BFF",
+        secondary: "#FFA500",
+        lightGray: "#F7F7F7",
+        mediumGray: "#B0B0B0",
+        actionAmber: "#FFBF00",
+        actionRed: "#FF3B30",
+        error: "#DC3545",
+        success: "#28A745",
+        textPrimary: "#333333",
+        textSecondary: "#666666",
+      },
+      fontFamily: {
+        primary: ["Montserrat", "sans-serif"],
+        secondary: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        h1: "48px",
+        h2: "36px",
+        h3: "28px",
+        body: ["16px", "18px"],
+        button: ["14px", "16px"],
+      },
+      borderRadius: {
+        DEFAULT: "0.375rem",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
