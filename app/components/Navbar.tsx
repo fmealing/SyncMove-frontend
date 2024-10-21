099ad2ec61bc9233e0ab1923dade65757d88bcbc
Florian Mealing <florianmealing@gmail.com>
2024-10-21 19:45:35 +0100

    Made the Navbar act dependent on if the user is logged in

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,32 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaUser, FaUserPlus } from "react-icons/fa";
+import { jwtDecode } from "jwt-decode"; // You might already have this installed
 
 const Navbar: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  // Check if the user is authenticated (i.e., if there's a valid token)
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        if (decodedToken) {
+          setIsAuthenticated(true); // Valid token, user is authenticated
+        }
+      } catch (error) {
+        setIsAuthenticated(false); // Invalid token, user is not authenticated
+      }
+    }
+  }, []);
+
   return (
     <nav className="flex items-center justify-between px-10 py-4 bg-lightGray shadow-sm">
       {/* Logo */}
       <div className="flex items-center space-x-2">
-        <Link href="/">
+        <Link href={isAuthenticated ? "/dashboard" : "/"}>
           <img src="/logo.png" alt="SyncMove Logo" className="h-[63px]" />
         </Link>
       </div>
@@ -24,7 +43,7 @@ const Navbar: React.FC = () => {
           </Link>
         </button>
         <button>
-          <Link href="/FAQ">
+          <Link href="/contact">
             <div className="h-9 px-4 py-2 rounded-full border-2 border-secondary justify-center items-center gap-2 inline-flex hover:border-secondaryDark transition">
               <div className="text-center text-secondary text-lg font-semibold font-primary leading-[17.50px] hover:text-secondaryDark transition">
                 Contact
@@ -59,7 +78,6 @@ const Navbar: React.FC = () => {
             <FaUser className="mr-2" /> Login
           </button>
         </Link>
-        {/* TODO: Implement signup page which redirects to onboarding page */}
         <Link href="/signup">
           <button className="flex items-center px-4 py-2 bg-primary text-lightGray rounded-full font-bold font-secondary text-lg hover:bg-primaryDark transition">
             <FaUserPlus className="mr-2" /> Signup
