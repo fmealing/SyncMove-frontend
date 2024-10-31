b0f70edce5660c9c8e229d6fce5f456f698905da
Florian Mealing <florianmealing@gmail.com>
2024-10-31 20:15:19 +0000

    Added Login with Google button

--- app/components/LoginWithGoogle.tsx
+++ app/components/LoginWithGoogle.tsx
@@ -1,14 +1,15 @@
+"use client";
 import React from "react";
+import { signIn } from "next-auth/react";
 
-const LoginWithGoogle = () => {
-  const handleLogin = () => {
-    // Redirect the user to the Google OAuth route on the backend
-    window.location.href = "http://localhost:5001/api/auth/callback/google";
+const LoginWithGoogle: React.FC = () => {
+  const handleGoogleLogin = () => {
+    signIn("google");
   };
 
   return (
     <button
-      onClick={handleLogin}
+      onClick={handleGoogleLogin}
       className="w-full max-w-md h-12 px-4 rounded-full border-2 border-textPrimary flex justify-center items-center gap-2 mb-4 hover:bg-actionAmber/5 transition"
     >
       <img src="/svg/google-logo.svg" alt="Google Logo" className="w-12 h-12" />
